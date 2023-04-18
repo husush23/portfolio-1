09cd92fdba9432d0c6a92852d65f6fe8829b496d
husush23 <husush21@gmail.com>
2023-04-18 10:19:49 +0300

    added js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const mobileHam = document.querySelector('.menu-m');
+const mobileMenu = document.querySelector('.mobile-menu');
+const elSize = document.querySelector('.introduction');
+
+// Open
+function openMenu() {
+  mobileHam.classList.add('hidden');
+  mobileMenu.classList.remove('hidden');
+}
